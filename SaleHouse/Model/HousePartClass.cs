4210e6c1aa044e17c3e4ab23dc37c03b4f727eac
epxch <snigir_00@mail.ru>
2021-11-11 15:21:08 +0300

    Добавьте файлы проекта.

---
+++ SaleHouse/Model/HousePartClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleHouse.Model
+{
+    public partial class House
+    {
+        public string GetHouseComplex
+        {
+            get
+            {
+                string complex = HouseComplex.Name + " | " + Street.Name + " | " + HouseComplex.BuildStatus.Name;
+                return complex;
+            }
+        }
+
+        public string GetHouse
+        {
+            get
+            {
+                int amountSale = Context._con.Apartment.Where(p => p.SaleStatusId == 1).Count();
+                int amontReady = Context._con.Apartment.Where(p => p.SaleStatusId == 2).Count(); 
+                string house = Number + " | Продано: " + amountSale + " | Готово к продаже: " + amontReady;
+                return house;
+            }
+        }
+    }
+}
---
+++ SaleHouse/Windows/HouseListing.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SaleHouse.Model;
+
+namespace SaleHouse.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для HouseListing.xaml
+    /// </summary>
+    public partial class HouseListing : Window
+    {
+        List<House> houses = new List<House>();
+
+        public HouseListing()
+        {
+
+            InitializeComponent();
+            HouseList.ItemsSource = Context._con.House.ToList();
+            HouseComplexCB.ItemsSource = Context._con.HouseComplex.ToList();
+            AddressCB.ItemsSource = Context._con.Street.ToList();
+            
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void HouseComplexCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void AddressCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        public void Filter()
+        {
+            houses = Context._con.House.ToList();
+            if (AddressCB.SelectedIndex >= 0)
+            {
+                houses = houses.Where(p => p.StreetId == AddressCB.SelectedIndex + 1).ToList();
+            }
+            HouseList.ItemsSource = houses;
+
+            if (HouseComplexCB.SelectedIndex >= 0)
+            {
+                houses = houses.Where(p => p.HouseComplexId == HouseComplexCB.SelectedIndex + 1).ToList();
+            }
+            HouseList.ItemsSource = houses;
+        }
+
+        private void DeleteHouse(object sender, RoutedEventArgs e)
+        {
+            ShowMessage.InfMesssage("Окно в разработке!");
+        }
+    }
+}
